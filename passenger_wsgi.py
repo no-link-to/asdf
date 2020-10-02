8f0f625b5965862958226461c905b8fa9560cd77
no_link_to <no.link.to@gmail.com>
2020-10-02 23:57:48 +0300

    new new new

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/var/www/u1105392/data/www/icalegal.ru/project_name')
+sys.path.insert(0, '/var/www/u1105392/data/www/icalegal.ru/asdf')
 sys.path.insert(1, '/var/www/u1105392/data/djangoenv/lib/python3.7/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project_name.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'asdf.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
